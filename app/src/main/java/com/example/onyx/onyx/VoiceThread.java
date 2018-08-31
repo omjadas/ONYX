a97aaf0596a3738733c0cbed7a3cbe1f3a689233
omjadas <omja.das@gmail.com>
2018-08-31 15:18:08 +1000

    setup VoiceService

--- app/src/main/java/com/example/onyx/onyx/VoiceThread.java
+++ app/src/main/java/com/example/onyx/onyx/VoiceThread.java
@@ -1,7 +1,33 @@
 package com.example.onyx.onyx;
 
+import android.app.Service;
+import android.content.Intent;
+import android.net.sip.SipManager;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 import java.lang.Thread;
 
-public class VoiceThread extends Thread {
+public class VoiceService extends Service {
+    private SipManager mSipManager = null;
+    public
 
+    VoiceService() {
+    }
+
+    public void onStartCommand() {
+        if (mSipManager == null) {
+            mSipManager = SipManager.newInstance(this);
+        }
+    }
+
+    public void shutdown() {
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
