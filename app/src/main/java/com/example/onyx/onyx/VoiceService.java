da37aec65dcf4b8505bada440b6d940ee0c414b2
omjadas <omja.das@gmail.com>
2018-08-31 16:32:00 +1000

    added callback methods

--- app/src/main/java/com/example/onyx/onyx/VoiceService.java
+++ app/src/main/java/com/example/onyx/onyx/VoiceService.java
@@ -3,26 +3,38 @@ package com.example.onyx.onyx;
 import android.app.Service;
 import android.content.Intent;
 import android.net.sip.SipManager;
+import android.net.sip.SipProfile;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import java.lang.Thread;
 
 public class VoiceService extends Service {
     private SipManager mSipManager = null;
-    public
+    public SipProfile mSipProfile = null;
 
-    VoiceService() {
-    }
-
-    public void onStartCommand() {
+    public void onCreate() {
         if (mSipManager == null) {
             mSipManager = SipManager.newInstance(this);
         }
     }
 
-    public void shutdown() {
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
 
+    public void onDestroy() {
+        if (mSipManager == null) {
+            return;
+        }
+        try {
+            if (mSipProfile != null) {
+                mSipManager.close(mSipProfile.getUriString());
+            }
+        } catch (Exception ee) {
+            Log.d("onDestroy", "Failed to close local profile.", ee);
+        }
     }
 
     @Nullable
