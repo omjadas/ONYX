aa4a68849d9d807d5ab7e5170fc91a054fa1b577
Taylbr <taylbr97@gmail.com>
2018-09-07 02:32:40 +1000

    Prototype sign in and sign up

--- app/src/main/java/com/example/onyx/onyx/SignInActivity.java
+++ app/src/main/java/com/example/onyx/onyx/SignInActivity.java
@@ -3,20 +3,45 @@ package com.example.onyx.onyx;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends Activity {
     private FirebaseAuth mAuth;
 
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText emailText, password;
+
+    private Button buttonSend;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
         mAuth = FirebaseAuth.getInstance();
+
+        emailText = (EditText) findViewById(R.id.emailText);
+        password = (EditText) findViewById(R.id.password);
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+
+        buttonSend.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                SignIn(emailText.getText().toString().trim(), password.getText().toString().trim());
+            }
+        });
+
     }
 
     @Override
@@ -24,6 +49,9 @@ public class SignInActivity extends Activity {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
+
+
+
     }
 
     private void SignIn(String email, String password){
@@ -36,15 +64,20 @@ public class SignInActivity extends Activity {
                 }
                 else{
                     updateUI(null);
+                    FirebaseAuthException e = (FirebaseAuthException )task.getException();
+                    System.out.println("Failed Registration: "+e.getMessage());
+                    return;
                 }
             }
         });
     }
     private void updateUI(FirebaseUser currentUser){
         if(currentUser == null){
+            System.out.println("nope");
             //need login screen
         }
         else{
+            System.out.println("logged in");
             //signed in
         }
     }
--- app/src/main/java/com/example/onyx/onyx/SignUpActivity.java
+++ app/src/main/java/com/example/onyx/onyx/SignUpActivity.java
@@ -1,14 +1,20 @@
 package com.example.onyx.onyx;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends FragmentActivity {
@@ -24,6 +30,7 @@ public class SignUpActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        System.out.println("test");
         mAuth = FirebaseAuth.getInstance();
         sendButton = findViewById(R.id.buttonSend);
         name = findViewById(R.id.nameText);
@@ -37,10 +44,32 @@ public class SignUpActivity extends FragmentActivity {
                 if(checkData(name.getText().toString(), password1.getText().toString(),
                         password2.getText().toString(), email.getText().toString(),
                         phoneNum.getText().toString())){
-
+                    createAccount(email.getText().toString().trim(), password1.getText().toString().trim());
                 }
             }
         });
+    }
+
+    private void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            System.out.println("Signed in");
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            System.out.println("nope");
+                            FirebaseAuthException e = (FirebaseAuthException )task.getException();
+                            System.out.println("Failed Registration: "+e.getMessage());
+                            return;
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     public boolean checkData(String name, String password1, String password2, String email, String phoneNum){
@@ -48,8 +77,8 @@ public class SignUpActivity extends FragmentActivity {
         if(name.isEmpty()){
 
         }
-        if(validPassword(password1, password2)){
-
+        if(!validPassword(password1, password2)){
+            valid = false;
         }
         if(email.isEmpty()){
 
