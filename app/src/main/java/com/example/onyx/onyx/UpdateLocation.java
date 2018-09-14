4e0fe462225c24d6be46a0c6ea0cda753fed2f19
omjadas <omja.das@gmail.com>
2018-09-14 16:40:31 +1000

    changed UpdateLocation to Service

--- app/src/main/java/com/example/onyx/onyx/UpdateLocation.java
+++ app/src/main/java/com/example/onyx/onyx/UpdateLocation.java
@@ -1,28 +1,49 @@
 package com.example.onyx.onyx;
 
+import android.annotation.SuppressLint;
 import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static android.support.constraint.Constraints.TAG;
 
-public class UpdateLocation extends IntentService {
+@SuppressLint("Registered")
+public class UpdateLocation extends Service {
 
-    public UpdateLocation(String name) {
-        super(name);
+    public void onCreate() {
+
     }
 
-    @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
+    public int onStartCommand(Intent intent, int flags, int startId) {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
-
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            Map<String, Object> location = new HashMap<>();
+            db.collection("users").document(user.getUid()).set(location);
         } else {
-            Log.e(TAG, "onHandleIntent: user not logged in", );
+            Log.e(TAG, "onHandleIntent: user not logged in");
         }
+
+        return START_STICKY;
+    }
+
+    public void onDestroy() {
+        Log.e("LocationExit", "Location service has been destroyed");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
