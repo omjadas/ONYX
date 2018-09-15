32cc493b29458af8f4232a8bdf0502a2bc53f288
Xin Wei Ding <xind1@student.unimelb.edu.au>
2018-09-15 21:47:13 +1000

    contacts page now uses forestore user collection

--- app/src/main/java/com/example/onyx/onyx/core/users/get/all/GetUsersInteractor.java
+++ app/src/main/java/com/example/onyx/onyx/core/users/get/all/GetUsersInteractor.java
@@ -1,5 +1,6 @@
 package com.example.onyx.onyx.core.users.get.all;
 
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
 import com.example.onyx.onyx.models.User;
@@ -13,11 +14,20 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class GetUsersInteractor implements GetUsersInterface.Interactor {
     private static final String TAG = "GetUsersInteractor";
 
     private GetUsersInterface.OnGetAllUsersListener mOnGetAllUsersListener;
+    private FirebaseFirestore db;
+    private CollectionReference users;
 
     public GetUsersInteractor(GetUsersInterface.OnGetAllUsersListener onGetAllUsersListener) {
         this.mOnGetAllUsersListener = onGetAllUsersListener;
@@ -26,26 +36,30 @@ public class GetUsersInteractor implements GetUsersInterface.Interactor {
 
     @Override
     public void getAllUsersFromFirebase() {
-        FirebaseDatabase.getInstance().getReference().child(Constants.ARG_USERS).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Iterator<DataSnapshot> dataSnapshots = dataSnapshot.getChildren().iterator();
-                List<User> users = new ArrayList<>();
-                while (dataSnapshots.hasNext()) {
-                    DataSnapshot dataSnapshotChild = dataSnapshots.next();
-                    User user = dataSnapshotChild.getValue(User.class);
-                    if (!TextUtils.equals(user.uid, FirebaseAuth.getInstance().getCurrentUser().getUid())) {
-                        users.add(user);
-                    }
-                }
-                mOnGetAllUsersListener.onGetAllUsersSuccess(users);
-            }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                mOnGetAllUsersListener.onGetAllUsersFailure(databaseError.getMessage());
-            }
-        });
+        db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            List<DocumentSnapshot> myListOfDocuments = task.getResult().getDocuments();
+
+                            List<User> users = new ArrayList<>();
+                            for (DocumentSnapshot dss : myListOfDocuments) {
+
+                                User user = dss.toObject(User.class);
+                                if (!TextUtils.equals(user.uid, FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        && !user.email.equals(FirebaseAuth.getInstance().getCurrentUser().getEmail())) {
+                                    users.add(user);
+                                }
+                            }
+                            mOnGetAllUsersListener.onGetAllUsersSuccess(users);
+                        }
+                    }
+                });
+
     }
 
     @Override
--- app/src/main/java/com/example/onyx/onyx/models/User.java
+++ app/src/main/java/com/example/onyx/onyx/models/User.java
@@ -9,13 +9,19 @@ public class User {
     public String uid;
     public String email;
     public String firebaseToken;
+    public String firstName;
+    public String lastName;
+    public boolean isOnline;
 
     public User() {
     }
 
-    public User(String uid, String email, String firebaseToken) {
+    public User(String uid, String email, String firebaseToken,String firstName,String lastName,boolean isOnline) {
         this.uid = uid;
         this.email = email;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.isOnline = isOnline;
         this.firebaseToken = firebaseToken;
     }
 }
