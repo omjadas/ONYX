5a178df31ac7d80106c73e9c3c67addc23cfe790
Sameer Asim <asims@student.unimelb.edu.au>
2018-10-10 14:46:50 +1100

    #210 split permissions into separate methods

--- app/src/main/java/com/example/onyx/onyx/Permissions.java
+++ app/src/main/java/com/example/onyx/onyx/Permissions.java
@@ -7,40 +7,40 @@ import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
-import static com.example.onyx.onyx.MapsFragment.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
-
 public class Permissions {
 
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final int PERMISSIONS_REQUEST_ACCESS_MIC = 1;
+    private static final int PERMISSIONS_REQUEST_ACCESS_CAMERA = 1;
     private static final int CAMERA_MIC_PERMISSION_REQUEST_CODE = 1;
-    public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     public static void getPermissions(Context context, Activity activity) {
-        // location permission
-        if (ContextCompat.checkSelfPermission(context,
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(activity,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-
-        }
-
-        // audio and video permissions
-        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.CAMERA) ||
-                ActivityCompat.shouldShowRequestPermissionRationale(activity,
-                        Manifest.permission.RECORD_AUDIO)) {
-        } else {
-            ActivityCompat.requestPermissions(
-                    activity,
-                    new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO},
-                    CAMERA_MIC_PERMISSION_REQUEST_CODE);
-        }
+        getLocationPermission(context, activity);
+        getVoicePermission(context, activity);
+        getCameraPermission(context, activity);
     }
 
     public static void getLocationPermission(Context context, Activity activity){
         if (!hasLocationPermission(context, activity)){
             ActivityCompat.requestPermissions(activity,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    public static void getVoicePermission(Context context, Activity activity){
+        if (!hasLocationPermission(context, activity)){
+            ActivityCompat.requestPermissions(activity,
+                    new String[]{android.Manifest.permission.RECORD_AUDIO},
+                    PERMISSIONS_REQUEST_ACCESS_MIC);
+        }
+    }
+
+    public static void getCameraPermission(Context context, Activity activity){
+        if (!hasLocationPermission(context, activity)){
+            ActivityCompat.requestPermissions(activity,
+                    new String[]{android.Manifest.permission.CAMERA},
+                    PERMISSIONS_REQUEST_ACCESS_CAMERA);
         }
     }
 
@@ -52,7 +52,7 @@ public class Permissions {
         return ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
 
-    public static boolean hasVideoPermission(Context context, Activity activity){
+    public static boolean hasCameraPermission(Context context, Activity activity){
         return ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
 }
